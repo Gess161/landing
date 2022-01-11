a75ee823ed196f8575cf41c23a724043b6e729a1
Gess <newerr161@gmail.com>
2022-01-11 15:20:32 +0200

    fixed background streching and clouds

--- components/Background/Clouds.js
+++ components/Background/Clouds.js
@@ -4,80 +4,84 @@ import Image from "next/image";
 export default function Clouds() {
     return (
         <Box
+            display="flex"
             width="100%"
             height="100%"
             overflow="hidden"
             position="relative"
+            top="0"
+            left="0"
             sx={{
                 backgroundColor: "rgb(152, 191, 199);",
             }}
         >
             <Box
+                position="absolute"
                 width="100%"
-                height="50%"
+                height="100%"
                 sx={{ background: "linear-gradient(330.24deg, rgba(11, 29, 38, 0) 31.06%, #0B1D26 108.93%);" }}
                 zIndex={10}
             >
             </Box>
             <Box
-                top="-50px"
+                top="-26%"
                 left="0"
                 position="absolute"
                 width="100%"
-                height="50%"
+                height="90%"
                 sx={{
                     animation: "animatemid 120s linear infinite"
                 }}
             >
-                <Image src={'/clouds/cloud1.png'} priority layout="fill" />
+                <Image src={'/clouds/cloud1.png'} layout="fill" />
             </Box>
             <Box
-                top="-50px"
+                top="-19%"
                 left="0"
                 position="absolute"
                 width="100%"
-                height="50%"
+                height="90%"
                 sx={{
                     animation: "animate 140s linear infinite"
                 }}
             >
-                <Image src={'/clouds/cloud2.png'} priority layout="fill" />
+                <Image src={'/clouds/cloud2.png'} layout="fill" />
             </Box>
             <Box
                 sx={{
                     animation: "animate 200s linear infinite"
                 }}
-                top="-50px"
+                top="-19%"
                 left="0"
                 width="100%"
-                height="50%"
+                height="90%"
                 position="absolute"
             >
-                <Image src={'/clouds/cloud3.png'} priority layout="fill" />
+                <Image src={'/clouds/cloud3.png'} layout="fill" />
             </Box>
             <Box
                 sx={{
                     animation: "animatemid 280s linear infinite"
                 }}
                 width="100%"
-                height="50%"
+                height="90%"
                 position="absolute"
-                top="-50px"
+                top="-26%"
                 left="0"
             >
-                <Image src={'/clouds/cloud4.png'} priority layout="fill" />
+                <Image src={'/clouds/cloud4.png'}  layout="fill" />
             </Box>
             <Box
-                top="-50px"
+                top="-26%"
                 left="0"
                 width="100%"
-                height="50%"
+                height="90%"
                 position="absolute"
                 sx={{
                     animation: "animate 350s linear infinite"
                 }}
             >
-                <Image src={'/clouds/cloud5.png'} priority layout="fill" />
+                <Image src={'/clouds/cloud5.png'} layout="fill" />
             </Box>
         </Box>
     )
--- components/Background/index.js
+++ components/Background/index.js
@@ -7,57 +7,66 @@ export default function Background() {
         <Box
             position="absolute"
             width="100%"
-            height="1422px"
+            height="100%"
+            minHeight="1440px"
             z-index={-1}
+            sx={{
+                "@media(max-width:1180px)": {
+                    minHeight:"1000px"
+                }
+            }}
         >
             <Box
                 sx={{
                     display: "flex",
                     width: "100%",
                     height: "100%",
-                    background: "linear-gradient(340.24deg, hsla(200, 54%, 10%, 0), hsla(200, 54%, 10%, 0.4) 108.93%);",
+                    background: "linear-gradient(340.24deg, hsla(200, 54%, 10%, 0), hsla(200, 54%, 10%, 0.1) 108.93%);",
                     position: "absolute",
                     zIndex: 1
                 }}
             />
             <Clouds />
             <Box
-                height="21vw"
+                height="200px"
                 width="100%"
                 position="absolute"
-                top="74vw"
+                bottom='-5px'
                 sx={{ background: "linear-gradient(180deg, rgba(11, 29, 38, 0) 0%, #0B1D26 61.38%);" }}
                 zIndex={1}
-            >
-            </Box>
-            <Box
+            />
+            <Box 
+                position="absolute"
                 width="100%"
                 height="100%"
-                position="absolute"
-                top="18.2vw"
-
+                top='0'
+                left='0'
+                overflow="hidden"
             >
-                <Image
-                    layout="responsive"
+                <Box
                     width="100%"
-                    height="70%"
-                    src="/mnt.png"
-                    priority
-                />
-            </Box>
-            <Box
-                top="33.85vw"
-                width="100%"
-                height="100%"
-                position="absolute"
-            >
-                <Image
-                    layout="responsive"
-                    width="140%"
+                    height="100%"
+                    position="absolute"
+                    top="18.2vw"
+                >
+                    <Image
+                        layout="fill"
+                        src="/mnt.png"
+                        priority
+                    />
+                </Box>
+                <Box
+                    width="100%"
                     height="80%"
-                    src="/hill.png"
-                    priority
-                />
+                    position="absolute"
+                    top="33.85vw"
+                >
+                    <Image
+                        layout="fill"
+                        src="/hill.png"
+                        priority
+                    />
+                </Box>
             </Box>
         </Box>
     )
